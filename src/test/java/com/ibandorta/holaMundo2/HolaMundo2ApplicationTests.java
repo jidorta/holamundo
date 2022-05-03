ec23b71e664f99ff73f056f2d3ba80f4e881942a
jidorta <ibandorta@gmail.com>
2022-05-03 08:49:50 +0200

    Texto que identifique por que se hizo el commit

---
+++ src/test/java/com/ibandorta/holaMundo2/HolaMundo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibandorta.holaMundo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolaMundo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
